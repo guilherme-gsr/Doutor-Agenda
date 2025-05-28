598ded9773d57d069d9506e054906e983f6fae56
Guilherme S.R <guilhermesiqueira342@gmail.com>
2025-05-27 21:58:46 -0300

    database

---
+++ src/db/schema.ts
@@ -0,0 +1,14 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid("id").defaultRandom().primaryKey(),
+});
+
+export const clinicsTable = pgTable("clinics", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  name: text("name").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
