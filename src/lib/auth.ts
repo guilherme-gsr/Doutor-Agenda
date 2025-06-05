17cead92dd33182ae4b8acebc76897d774f7a02f
Guilherme S.R <guilhermesiqueira342@gmail.com>
2025-06-05 19:32:04 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,6 +11,13 @@ export const auth = betterAuth({
     schema,
   }),
 
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+
   user: {
     modelName: "usersTable",
   },
